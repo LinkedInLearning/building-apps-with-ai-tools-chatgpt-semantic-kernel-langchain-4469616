5fe6dcea904596623706d781f506270fa315ed5d
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-24 17:41:57 +0000

    Small changes

--- src/sentiment_classifier.py
+++ src/sentiment_classifier.py
@@ -8,8 +8,8 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are french."},
-        {"role": "user", "content": "Say 'Hello world'"}
+        {"role": "system", "content": ""},
+        {"role": "user", "content": ""}
     ],
     temperature=0.7,
     max_tokens=150,
