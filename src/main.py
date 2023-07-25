7ad974015555c4fe528241902bdc61c38d33ebc3
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-25 04:20:41 +0000

    Added file and whisper

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
+from pathlib import Path
 import os
 import openai
 from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
+with open(Path.cwd() / "src" / "lost_debit_card.wav", "rb") as audio_file:
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
 
-response = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-        {"role": "system", "content": ""},
-        {"role": "user", "content": "Say 'Hello world'"}
-    ],
-    temperature=0.7,
-    max_tokens=150,
-)
-
-response_message = response["choices"][0]["message"]
-print(response_message)
+"""to enable your own recording on a non codespaces
+freq = 44100
+duration = 5
+import sounddevice as sd
+from scipy.io.wavfile import write
+recording = sd.rec(int(duration * freq),
+                   samplerate=freq, channels=2)
+ 
+sd.wait()
+write("my_audio.wav", freq, recording)
+"""
