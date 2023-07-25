c5d6884af76a339c714c93b67638e5bfea50dd0f
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-25 03:59:54 +0000

    Added summarizer

---
+++ src/summarizer.py
@@ -0,0 +1,33 @@
+import os
+import openai
+import semantic_kernel as sk
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+
+kernel = sk.Kernel()
+
+# Prepare OpenAI service using credentials stored in the `.env` file
+api_key, org_id = sk.openai_settings_from_dot_env()
+kernel.add_text_completion_service(
+    "dv", OpenAIChatCompletion("gpt-4", api_key, org_id))
+
+prompt = kernel.create_semantic_function("""
+1) A robot may not injure a human being or, through inaction,
+allow a human being to come to harm.
+
+2) A robot must obey orders given it by human beings except where
+such orders would conflict with the First Law.
+
+3) A robot must protect its own existence as long as such protection
+does not conflict with the First or Second Law.
+
+Give me the TLDR in exactly 5 words.""")
+
+# Create a reusable function with one input parameter
+summarize = kernel.create_semantic_function(
+    "{{$input}}\n\nOne line TLDR with the fewest words.")
+
+# Summarize the laws of thermodynamics
+print(summarize("""
+1st Law of Thermodynamics - Energy cannot be created or destroyed.
+2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
+3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy."""))
