da472ae56923811027d3a6ccf7afdaa590f7cd11
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-25 04:21:38 +0000

    removed solution

--- src/main.py
+++ src/main.py
@@ -4,18 +4,3 @@ import openai
 from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
-with open(Path.cwd() / "src" / "lost_debit_card.wav", "rb") as audio_file:
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript)
-
-"""to enable your own recording on a non codespaces
-freq = 44100
-duration = 5
-import sounddevice as sd
-from scipy.io.wavfile import write
-recording = sd.rec(int(duration * freq),
-                   samplerate=freq, channels=2)
- 
-sd.wait()
-write("my_audio.wav", freq, recording)
-"""
