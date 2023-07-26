613038fc13b9faa1956582823f4d26bdc5ca10a9
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-26 03:46:25 +0000

    Final results

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import WebBaseLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+loader = WebBaseLoader("https://en.wikipedia.org/wiki/Tea")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+docsearch = FAISS.from_documents(texts, embeddings)
+
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
+
+while True:
+    query = input("Ask a question about tea\n")
+    print(qa.run(query))
