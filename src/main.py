31148a4ba4819b78a3583ca69b9c0f4df6f969ca
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-27 18:20:27 +0000

    Starter code

--- src/main.py
+++ src/main.py
@@ -14,25 +14,3 @@ class Person(BaseModel):
 
 class PeopleList(BaseModel):
     people: list[Person] = Field(description="A list of people")
-
-
-model = ChatOpenAI(model="gpt-4")
-people_data = model.predict(
-    "Generate a list of 10 fake peoples information. Only return the list. Each person should have a first name, last name and date of birth.")
-
-parser = PydanticOutputParser(pydantic_object=PeopleList)
-
-prompt = PromptTemplate(
-    template="Answer the user query.\n{format_instructions}\n{query}\n",
-    input_variables=["query"],
-    partial_variables={
-        "format_instructions": parser.get_format_instructions()},
-)
-
-_input = prompt.format_prompt(query=people_data)
-
-model = ChatOpenAI()
-output = model.predict(_input.to_string())
-
-parsed = parser.parse(output)
-print(parsed)
