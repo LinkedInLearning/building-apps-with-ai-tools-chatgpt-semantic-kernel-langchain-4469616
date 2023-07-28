ae957ba84fcf1a5e743d84fed05acf9df26d8d74
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-28 21:50:53 +0000

    Added

--- src/main.py
+++ src/main.py
@@ -1,101 +1,15 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.chains import LLMChain
-from langchain.evaluation import QAEvalChain
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+from services.ai import generate_review
+app = FastAPI()
 
 
-def generate_book_recommendations(book_requests):
-    """
-    Generate book recommendations based on user requests
-    """
-    # create templates
-    system_template_book_agent = """You are book recommendation agent. Provide a short recommendation based on the user request."""
-    system_message_prompt = SystemMessagePromptTemplate.from_template(
-        system_template_book_agent)
-
-    human_template_book_agent = "{text}"
-    human_message_prompt = HumanMessagePromptTemplate.from_template(
-        human_template_book_agent)
-
-    # create full prompt
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [system_message_prompt, human_message_prompt])
-
-    chain = LLMChain(
-        llm=ChatOpenAI(temperature=1),
-        prompt=chat_prompt
-    )
-
-    recommendations = []
-    for book_request in book_requests:
-        recommendations.append(chain.run(book_request))
-
-    return recommendations
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
 
 
-def generate_book_requests(n=5) -> list[str]:
-    """ Generate book requests
-    n: number of requests
-    """
-    # create templates
-    system_template_book_agent = """Generate one utterance for how someone may ask for a {text}. Include a genre and a year. """
-    system_message_prompt = SystemMessagePromptTemplate.from_template(
-        system_template_book_agent)
-
-    # create full prompt
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [system_message_prompt])
-
-    chain = LLMChain(
-        llm=ChatOpenAI(model='gpt-4'),
-        prompt=chat_prompt
-    )
-
-    results = []
-
-    for _ in range(0, n):
-        results.append(chain.run("book"))
-
-    return results
-
-
-# generate some requests
-book_requests = generate_book_requests()
-print("The book requests are:\n")
-print("\n".join(book_requests))
-print("\n")
-# get the recommendations
-print("The book recommendations are:\n")
-recommendations = generate_book_recommendations(book_requests)
-print("\n\n".join(recommendations))
-print("\n")
-
-
-llm = ChatOpenAI(model="gpt-4")
-predictions = []
-question_answers = []
-
-for i in range(0, len(recommendations)):
-    q = book_requests[i]
-    a = recommendations[i]
-    question_answers.append({"question": q, "answer": a})
-    response = llm.predict(
-        f"Generate the response to the question: {q}. Only print the answer.")
-    predictions.append({"result": {response}})
-
-print("\nGenerating Self eval:")
-
-# Start your eval chain
-eval_chain = QAEvalChain.from_llm(llm)
-
-# compare the results of two prompts against themselves
-graded_outputs = eval_chain.evaluate(question_answers,
-                                     predictions,
-                                     question_key="question",
-                                     prediction_key="result",
-                                     answer_key='answer')
-print(graded_outputs)
+@app.get("/api/gpt")
+async def gpt(review: str):
+    return generate_review(review)
---
+++ src/services/ai.py
@@ -0,0 +1,23 @@
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def generate_review(review):
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a sentiment classification bot, print out if the user is happy or sad. Only print out happy or sad."},
+            {"role": "user", "content": review}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
+
+    response_message = response["choices"][0]["message"]["content"]
+    if response_message == "happy":
+        return "Glad you enjoyed"
+
+    return "Here's a 20% discount code for next time DISCOUNT20"
