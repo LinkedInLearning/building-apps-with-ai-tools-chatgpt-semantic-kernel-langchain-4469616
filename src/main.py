066134854fe9f973ee4ad0d79092d755fdd3d18c
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-28 20:12:18 +0000

    Initial change

--- src/main.py
+++ src/main.py
@@ -8,18 +8,18 @@ from langchain.chains import LLMChain
 from langchain.schema import BaseOutputParser
 
 
-def generate_travel_recommendations(travel_requests):
+def generate_book_recommendations(book_requests):
     """
-    Generate travel recommendations based on user requests
+    Generate book recommendations based on user requests
     """
     # create templates
-    system_template_travel_agent = """You are travel recommendation agent. Provide a short recommendation based on the user request."""
+    system_template_book_agent = """You are book recommendation agent. Provide a short recommendation based on the user request."""
     system_message_prompt = SystemMessagePromptTemplate.from_template(
-        system_template_travel_agent)
+        system_template_book_agent)
 
-    human_template_travel_agent = "{text}"
+    human_template_book_agent = "{text}"
     human_message_prompt = HumanMessagePromptTemplate.from_template(
-        human_template_travel_agent)
+        human_template_book_agent)
 
     # create full prompt
     chat_prompt = ChatPromptTemplate.from_messages(
@@ -31,20 +31,20 @@ def generate_travel_recommendations(travel_requests):
     )
 
     recommendations = []
-    for travel_request in travel_requests:
-        recommendations.append(chain.run(travel_request))
+    for book_request in book_requests:
+        recommendations.append(chain.run(book_request))
 
     return recommendations
 
 
-def generate_travel_requests(n=5) -> list[str]:
-    """ Generate travel requests
+def generate_book_requests(n=5) -> list[str]:
+    """ Generate book requests
     n: number of requests
     """
     # create templates
-    system_template_travel_agent = """Generate one utterance for how someone would to travel for a {text}"""
+    system_template_book_agent = """Generate one utterance for how someone may ask for a {text}. Include a genre and a year. """
     system_message_prompt = SystemMessagePromptTemplate.from_template(
-        system_template_travel_agent)
+        system_template_book_agent)
 
     # create full prompt
     chat_prompt = ChatPromptTemplate.from_messages(
@@ -58,14 +58,18 @@ def generate_travel_requests(n=5) -> list[str]:
     results = []
 
     for _ in range(0, n):
-        results.append(chain.run("beach vacation"))
+        results.append(chain.run("book"))
 
     return results
 
 
 # generate some requests
-travel_requests = generate_travel_requests()
-print(travel_requests)
+book_requests = generate_book_requests()
+print("The book requests are:\n")
+print("\n".join(book_requests))
+print("\n")
 # get the recommendations
-recommendations = generate_travel_recommendations(travel_requests)
-print(recommendations)
+print("The book recommendations are:\n")
+recommendations = generate_book_recommendations(book_requests)
+print("\n\n".join(recommendations))
+print("\n")
