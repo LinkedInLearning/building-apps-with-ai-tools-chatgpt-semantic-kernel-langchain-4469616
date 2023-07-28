dd1cb238ae77f5a19d884ce11ca664fd5cefd29a
Diophontine <12723001+Diophontine@users.noreply.github.com>
2023-07-28 18:21:16 +0000

    Created starter code

--- src/main.py
+++ src/main.py
@@ -22,24 +22,3 @@ loader = CSVLoader(
     file_path="./src/dataset_small.csv", source_column="title")
 
 data = loader.load()
-
-embeddings = OpenAIEmbeddings()
-
-quadrant_docsearch = Qdrant.from_documents(
-    data,
-    embeddings,
-    location=":memory:",
-    collection_name="book"
-)
-
-qa = RetrievalQA.from_chain_type(
-    llm=OpenAI(), chain_type="stuff", retriever=quadrant_docsearch.as_retriever(), return_source_documents=True)
-
-while True:
-    user_input = input("Hi im an AI librarian what can I help you with?\n")
-
-    book_request = "You are a librarian. Help the user answer their question. Do not provide the ISBN." +\
-        f"\nUser:{user_input}"
-    result = qa({"query": book_request})
-    print(len(result['source_documents']))
-    print(result["result"])
